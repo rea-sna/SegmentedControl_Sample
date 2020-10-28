033ad52c2139bbc020c8619bb368f25e02de62e7
Reo Shigeno <reoshigeno0708@gmail.com>
2020-10-28 22:46:54 +0900

    Initial Commit

---
+++ SegmentedControl_Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControl_Sample
+//
+//  Created by 繁野怜央 on 20201028.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
